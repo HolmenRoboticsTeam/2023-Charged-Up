eaaa2bb2ed2353d7c1037c35602cd576d532f585
vuenat23 <vuenat23@students.holmen.k12.wi.us>
2023-01-24 20:37:17 -0600

    Path and Events subsystem & commands added

---
+++ src/main/java/frc/robot/autonomous/RobotWaitSubsystem.java
@@ -0,0 +1,20 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.autonomous;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class RobotWaitSubsystem extends SubsystemBase {
+  /** Creates a new RobotWaitSubsystem. */
+  public RobotWaitSubsystem() {
+    // code from website that allows program to follow path
+    
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}
