bfffe90cfd4d4aa1ff696dd0feb26b50b8cdd1fe
vuenat23 <vuenat23@students.holmen.k12.wi.us>
2023-02-06 20:24:41 -0600

    Merge pull request #16 from HolmenRoboticsTeam/autonomous Autonomous. Read alot about mechanisms and how to simulate them.

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -5,6 +5,9 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.smartdashboard.Mechanism2d;
+import edu.wpi.first.wpilibj.smartdashboard.MechanismLigament2d;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 
