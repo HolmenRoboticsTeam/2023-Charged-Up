a7e733374141a6ee2067c79db5a4d7aa7d26b24f
Liam-Maricle <wgmaricle@gmail.com>
2023-04-27 19:32:57 -0500

    Setup Advantage Kit

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -15,6 +15,7 @@ package frc.robot;
 
 import java.util.HashMap;
 
+import com.fasterxml.jackson.annotation.JsonCreator.Mode;
 import com.pathplanner.lib.PathConstraints;
 import com.pathplanner.lib.auto.PIDConstants;
 import com.revrobotics.CANSparkMax.IdleMode;
@@ -24,6 +25,7 @@ import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
 import edu.wpi.first.math.trajectory.TrapezoidProfile;
 import edu.wpi.first.math.util.Units;
+import edu.wpi.first.wpilibj.RobotBase;
 import edu.wpi.first.wpilibj2.command.Command;
 
 /**
@@ -39,10 +41,22 @@ import edu.wpi.first.wpilibj2.command.Command;
  * constants are needed, to reduce verbosity.
  */
 public final class Constants {
+  public static final Mode currentMode = RobotBase.isReal() ? Mode.REAL : Mode.SIM;
+
+  public static enum Mode {
+    /** Running on a real robot. */
+    REAL,
+
+    /** Running a physics simulator. */
+    SIM,
+
+    /** Replaying from a log file. */
+    REPLAY
+  }
 
   public static final class AutonomousConstants {
-    public static final double kMaxVelocity = 1.25;                   //max velocity in 4 m/s
-    public static final double kMaxAcceleration = 3.0;               //max acceleration in 3 m/s^2
+    public static final double kMaxVelocity = 1.25; // max velocity in 4 m/s
+    public static final double kMaxAcceleration = 3.0; // max acceleration in 3 m/s^2
 
     public static final HashMap<String, Command> eventMap = new HashMap<>();
 
@@ -70,12 +84,14 @@ public final class Constants {
     public static final int kPivotCanId = 11;
 
     // Calculations required for extension motor conversion factors and feed forward
-    // Drum diameter is averaged between fully contracted and fully extended wraps of the rope
+    // Drum diameter is averaged between fully contracted and fully extended wraps
+    // of the rope
     public static final double kExtendMotorFreeSpeedRps = NeoMotorConstants.kFreeSpeedRpm / 60;
     public static final double kPivotMotorFreeSpeedRps = Neo550MotorConstants.kFreeSpeedRpm / 60;
     public static final double kDrumDiameterMeters = 0.0259; // Units.inchesToMeters(1.02);
     public static final double kDrumCircumferenceMeters = kDrumDiameterMeters * Math.PI;
-    // Setting up motor reductions for extention and pivot. Using FreeSpeedRPS to calculate FF for the pivot motor.
+    // Setting up motor reductions for extention and pivot. Using FreeSpeedRPS to
+    // calculate FF for the pivot motor.
     public static final double kExtendMotorReduction = 1.0;
     public static final double kPivotMotorReduction = (46.0 / 25.0) * 100.0;
     public static final double kPivotArmFreeSpeedRps = (kPivotMotorFreeSpeedRps * kDrumCircumferenceMeters)
@@ -83,8 +99,9 @@ public final class Constants {
 
     public static final double kExtendEncoderCPR = 8192.0;
     public static final double kExtendEncoderPositionFactor = kDrumCircumferenceMeters / kExtendEncoderCPR; // Meters
-    public static final double kFullyContractedLengthMeters = 1.035;  // Middle of drum to tip of gripper (rope length); Units.inchesToMeters(40.75);
-    public static final double kGripperTipToMiddleOffsetMeters = 0.083;  // Units.inchesToMeters(3.25);
+    public static final double kFullyContractedLengthMeters = 1.035; // Middle of drum to tip of gripper (rope length);
+                                                                     // Units.inchesToMeters(40.75);
+    public static final double kGripperTipToMiddleOffsetMeters = 0.083; // Units.inchesToMeters(3.25);
 
     // Encoder is on the output shaft
     public static final double kPivotEncoderPositionFactor = 360.0;
@@ -95,13 +112,13 @@ public final class Constants {
     public static final Rotation2d kPivotForwardLimit = Rotation2d.fromDegrees(100);
     public static final Rotation2d kPivotReverseLimit = Rotation2d.fromDegrees(-10);
 
-    public static final double kPivotEncoderPositionPIDMinInput = 0;  // Radians
-    public static final double kPivotEncoderPositionPIDMaxInput = kPivotEncoderPositionFactor;  // Radians
+    public static final double kPivotEncoderPositionPIDMinInput = 0; // Radians
+    public static final double kPivotEncoderPositionPIDMaxInput = kPivotEncoderPositionFactor; // Radians
 
     public static final double kPivotRampRateInSeconds = 3.0;
 
     public static final double kExtendP = 1e-4;
-    public static final double kExtendI = 0.0; //1e-6;
+    public static final double kExtendI = 0.0; // 1e-6;
     public static final double kExtendD = 0;
     public static final double kExtendFF = 0.000156;
     public static final double kExtendMinOutput = -1;
@@ -112,7 +129,7 @@ public final class Constants {
     public static final double kExtendAllowedError = 0;
 
     public static final double kPivotP = 1e-4;
-    public static final double kPivotI = 0.0; //1e-6;
+    public static final double kPivotI = 0.0; // 1e-6;
     public static final double kPivotD = 0;
     public static final double kPivotFF = 0.000156;
     public static final double kPivotMinOutput = -1;
@@ -128,33 +145,34 @@ public final class Constants {
     public static final int kExtendMotorCurrentLimit = 20;
     public static final int kPivotMotorCurrentLimit = 20;
 
-    public static final double kMaxHorizontalDistanceOffRobot = 59.5;  // Units.inchesToMeters(59.5)
+    public static final double kMaxHorizontalDistanceOffRobot = 59.5; // Units.inchesToMeters(59.5)
     public static final double kMinHorizontalDistanceOffRobot = 0.0;
 
     public static final boolean kExtendEncoderInverted = false;
 
   }
-// Angle and length of arm depending on button pressed. These are not the correct values
+  // Angle and length of arm depending on button pressed. These are not the
+  // correct values
 
   public static final class ArmPositionConstants {
-  public static final double kHomeAngle = 2;//Units.degreesToRadians(0);
-  public static final double kPickUpFromFloorAngle = 23;
-  public static final double kPickUpFromDriverStationAngle = 90; //.degreesToRadians(90);
-  public static final double kPlaceOnFloorAngle = 40;
-  public static final double kPlaceCubeOnLevel1Angle = 100;
-  public static final double kPlaceCubeOnLevel2Angle = 110;
-  public static final double kPlaceConeOnLevel1Angle = 100;
-  public static final double kPlaceConeOnLevel2Part1Angle = 80;
-  public static final double kPlaceConeOnLevel2Part2Angle = 110;
+    public static final double kHomeAngle = 2;// Units.degreesToRadians(0);
+    public static final double kPickUpFromFloorAngle = 23;
+    public static final double kPickUpFromDriverStationAngle = 90; // .degreesToRadians(90);
+    public static final double kPlaceOnFloorAngle = 40;
+    public static final double kPlaceCubeOnLevel1Angle = 100;
+    public static final double kPlaceCubeOnLevel2Angle = 110;
+    public static final double kPlaceConeOnLevel1Angle = 100;
+    public static final double kPlaceConeOnLevel2Part1Angle = 80;
+    public static final double kPlaceConeOnLevel2Part2Angle = 110;
 
-  public static final double kHomeBoomLength = 0.0;
-  public static final double kPickUpFromFloorBoomLength = 140.0;
-  public static final double kPickUpFromDriverStationBoomLength = 20.0;
-  public static final double kPlaceOnFloorBoomLength = 0;
-  public static final double kPlaceCubeOnLevel1BoomLength = 25.0;
-  public static final double kPlaceCubeOnLevel2BoomLength = 0;
-  public static final double kPlaceConeOnLevel1BoomLength = 25.0;
-  public static final double kPlaceConeOnLevel2BoomLength = 230.0;
+    public static final double kHomeBoomLength = 0.0;
+    public static final double kPickUpFromFloorBoomLength = 140.0;
+    public static final double kPickUpFromDriverStationBoomLength = 20.0;
+    public static final double kPlaceOnFloorBoomLength = 0;
+    public static final double kPlaceCubeOnLevel1BoomLength = 25.0;
+    public static final double kPlaceCubeOnLevel2BoomLength = 0;
+    public static final double kPlaceConeOnLevel1BoomLength = 25.0;
+    public static final double kPlaceConeOnLevel2BoomLength = 230.0;
   }
 
   public static final class DriveConstants {
@@ -162,7 +180,7 @@ public final class Constants {
     // the robot, rather the allowed maximum speeds
     public static final double kMaxSpeedMetersPerSecond = 4.0; // 4.8 IS MAX SPEED
     public static final double kMaxAngularSpeed = 2.0 * Math.PI; // radians per second
-    public static final double kMaxAnguolarSpeedSquared = 2.0 * Math.PI; //radians per secondF
+    public static final double kMaxAnguolarSpeedSquared = 2.0 * Math.PI; // radians per secondF
     public static final double kSwerveTargetingOffset = 630.0; // Degrees
 
     public static final double kDirectionSlewRate = 1.2; // radians per second
@@ -186,15 +204,13 @@ public final class Constants {
     public static final double kHeadingI = 0.0;
     public static final double kHeadingD = 0.0;
     public static final TrapezoidProfile.Constraints kHeadingControllerConstraints = new TrapezoidProfile.Constraints(
-      kMaxAngularSpeed, kMaxAnguolarSpeedSquared
-      );
+        kMaxAngularSpeed, kMaxAnguolarSpeedSquared);
 
     // Angular offsets of the modules relative to the chassis in radians
     public static final double kFrontLeftChassisAngularOffset = -Math.PI / 2;
     public static final double kFrontRightChassisAngularOffset = 0;
     public static final double kBackLeftChassisAngularOffset = Math.PI;
     public static final double kBackRightChassisAngularOffset = Math.PI / 2;
-
 
     // SPARK MAX CAN IDs
     public static final int kFrontLeftDrivingCanId = 5;
@@ -211,20 +227,24 @@ public final class Constants {
   }
 
   public static final class ModuleConstants {
-    // The MAXSwerve module can be configured with one of three pinion gears: 12T, 13T, or 14T.
-    // This changes the drive speed of the module (a pinion gear with more teeth will result in a
+    // The MAXSwerve module can be configured with one of three pinion gears: 12T,
+    // 13T, or 14T.
+    // This changes the drive speed of the module (a pinion gear with more teeth
+    // will result in a
     // robot that drives faster).
     public static final int kDrivingMotorPinionTeeth = 13;
 
-    // Invert the turning encoder, since the output shaft rotates in the opposite direction of
+    // Invert the turning encoder, since the output shaft rotates in the opposite
+    // direction of
     // the motor in the MAXSwerve Module.
     public static final boolean kTurningEncoderInverted = true;
 
     // Calculations required for driving motor conversion factors and feed forward
     public static final double kDrivingMotorFreeSpeedRps = NeoMotorConstants.kFreeSpeedRpm / 60;
-    public static final double kWheelDiameterMeters = 0.0762;  // Units.inchesToMeters(3);
+    public static final double kWheelDiameterMeters = 0.0762; // Units.inchesToMeters(3);
     public static final double kWheelCircumferenceMeters = kWheelDiameterMeters * Math.PI;
-    // 45 teeth on the wheel's bevel gear, 22 teeth on the first-stage spur gear, 15 teeth on the bevel pinion
+    // 45 teeth on the wheel's bevel gear, 22 teeth on the first-stage spur gear, 15
+    // teeth on the bevel pinion
     public static final double kDrivingMotorReduction = (45.0 * 22) / (kDrivingMotorPinionTeeth * 15);
     public static final double kDriveWheelFreeSpeedRps = (kDrivingMotorFreeSpeedRps * kWheelCircumferenceMeters)
         / kDrivingMotorReduction;
--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,60 +4,129 @@
 
 package frc.robot;
 
+import org.littletonrobotics.junction.LogFileUtil;
+import org.littletonrobotics.junction.LoggedRobot;
+import org.littletonrobotics.junction.Logger;
+import org.littletonrobotics.junction.networktables.NT4Publisher;
+import org.littletonrobotics.junction.wpilog.WPILOGReader;
+import org.littletonrobotics.junction.wpilog.WPILOGWriter;
+
 import com.revrobotics.AbsoluteEncoder;
 import com.revrobotics.RelativeEncoder;
 
-import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 
 /**
- * The VM is configured to automatically run this class, and to call the functions corresponding to
- * each mode, as described in the TimedRobot documentation. If you change the name of this class or
- * the package after creating this project, you must also update the build.gradle file in the
+ * The VM is configured to automatically run this class, and to call the
+ * functions corresponding to
+ * each mode, as described in the TimedRobot documentation. If you change the
+ * name of this class or
+ * the package after creating this project, you must also update the
+ * build.gradle file in the
  * project.
  */
-public class Robot extends TimedRobot {
+public class Robot extends LoggedRobot {
   private Command m_autonomousCommand;
   private RobotContainer m_robotContainer;
 
   /**
-   * This function is run when the robot is first started up and should be used for any
+   * This function is run when the robot is first started up and should be used
+   * for any
    * initialization code.
    */
   @Override
   public void robotInit() {
-    // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
-    // autonomous chooser on the dashboard.
+    Logger logger = Logger.getInstance();
+
+    // Record metadata
+    logger.recordMetadata("ProjectName", BuildConstants.MAVEN_NAME);
+    logger.recordMetadata("BuildDate", BuildConstants.BUILD_DATE);
+    logger.recordMetadata("GitSHA", BuildConstants.GIT_SHA);
+    logger.recordMetadata("GitDate", BuildConstants.GIT_DATE);
+    logger.recordMetadata("GitBranch", BuildConstants.GIT_BRANCH);
+    switch (BuildConstants.DIRTY) {
+      case 0:
+        logger.recordMetadata("GitDirty", "All changes committed");
+        break;
+      case 1:
+        logger.recordMetadata("GitDirty", "Uncomitted changes");
+        break;
+      default:
+        logger.recordMetadata("GitDirty", "Unknown");
+        break;
+    }
+
+    // Set up data receivers & replay source
+    switch (Constants.currentMode) {
+      // Running on a real robot, log to a USB stick
+      case REAL:
+        logger.addDataReceiver(new WPILOGWriter(""));
+        logger.addDataReceiver(new NT4Publisher());
+        break;
+
+      // Running a physics simulator, log to local folder
+      case SIM:
+        logger.addDataReceiver(new WPILOGWriter(""));
+        logger.addDataReceiver(new NT4Publisher());
+        break;
+
+      // Replaying a log, set up replay source
+      case REPLAY:
+        setUseTiming(false); // Run as fast as possible
+        String logPath = LogFileUtil.findReplayLog();
+        logger.setReplaySource(new WPILOGReader(logPath));
+        logger.addDataReceiver(new WPILOGWriter(LogFileUtil.addPathSuffix(logPath, "_sim")));
+        break;
+    }
+
+    // See http://bit.ly/3YIzFZ6 for more information on timestamps in AdvantageKit.
+    // Logger.getInstance().disableDeterministicTimestamps()
+
+    // Start AdvantageKit logger
+    logger.start();
+
+    // Instantiate our RobotContainer. This will perform all our button bindings,
+    // and put our autonomous chooser on the dashboard.
     m_robotContainer = new RobotContainer();
 
-    //initialize PID controller and encoder objects
   }
 
   /**
-   * This function is called every 20 ms, no matter the mode. Use this for items like diagnostics
+   * This function is called every 20 ms, no matter the mode. Use this for items
+   * like diagnostics
    * that you want ran during disabled, autonomous, teleoperated and test.
    *
-   * <p>This runs after the mode specific periodic functions, but before LiveWindow and
+   * <p>
+   * This runs after the mode specific periodic functions, but before LiveWindow
+   * and
    * SmartDashboard integrated updating.
    */
   @Override
   public void robotPeriodic() {
-    // Runs the Scheduler.  This is responsible for polling buttons, adding newly-scheduled
-    // commands, running already-scheduled commands, removing finished or interrupted commands,
-    // and running subsystem periodic() methods.  This must be called from the robot's periodic
+    // Runs the Scheduler. This is responsible for polling buttons, adding
+    // newly-scheduled
+    // commands, running already-scheduled commands, removing finished or
+    // interrupted commands,
+    // and running subsystem periodic() methods. This must be called from the
+    // robot's periodic
     // block in order for anything in the Command-based framework to work.
     CommandScheduler.getInstance().run();
   }
 
   /** This function is called once each time the robot enters Disabled mode. */
   @Override
-  public void disabledInit() {}
+  public void disabledInit() {
+  }
 
   @Override
-  public void disabledPeriodic() {}
+  public void disabledPeriodic() {
+  }
 
-  /** This autonomous runs the autonomous command selected by your {@link RobotContainer} class. */
+  /**
+   * This autonomous runs the autonomous command selected by your
+   * {@link RobotContainer} class.
+   */
   @Override
   public void autonomousInit() {
     m_autonomousCommand = m_robotContainer.getAutonomousCommand();
@@ -77,7 +146,8 @@ public class Robot extends TimedRobot {
 
   /** This function is called periodically during autonomous. */
   @Override
-  public void autonomousPeriodic() {}
+  public void autonomousPeriodic() {
+  }
 
   @Override
   public void teleopInit() {
@@ -92,7 +162,8 @@ public class Robot extends TimedRobot {
 
   /** This function is called periodically during operator control. */
   @Override
-  public void teleopPeriodic() {}
+  public void teleopPeriodic() {
+  }
 
   @Override
   public void testInit() {
@@ -102,5 +173,6 @@ public class Robot extends TimedRobot {
 
   /** This function is called periodically during test mode. */
   @Override
-  public void testPeriodic() {}
+  public void testPeriodic() {
+  }
 }
